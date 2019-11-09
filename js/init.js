aa9d79d5d2b1d6e3569764fe114e6dc2e96314d4
SlimeVidas <dahan.isaac@gmail.com>
2019-11-09 22:09:51 +0200

    second commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+    $('.sidenav').sidenav();
+  });
+  
+  
+
+   $('.modal').modal();
+   //$('.modal-trigger').modal();
+  // end of document ready
+})(jQuery); // end of jQuery name space
